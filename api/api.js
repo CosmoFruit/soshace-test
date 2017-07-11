e4329bfa476d6dd038a2ae7c86faafe90f3ae773
CosmoFruit <nutchfel@ya.ru>
2017-07-11 15:05:53 +0300

    Создан API
    протестировано

---
+++ api/api.js
@@ -0,0 +1,122 @@
+
+var express = require('express');
+var router = express.Router();
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+var Schema = mongoose.Schema;
+const db = require('./db-config.js');
+
+ 
+/* SET MODELS SCHEMES */
+var categorySchema = new Schema(
+	{ 
+    name: { 
+            type: String,
+            required: true
+          } 
+  },
+	{ versionKey: false }
+);
+
+var itemSchema = new Schema(
+	{	
+    name:       { 
+                  type: String,
+                  required: true
+                },
+ 		category:     String,
+    price_in:   { 
+                  type: Number,
+                  default: 0
+                },
+    price_out:  {   
+                  type: Number,
+                  default: 0
+                },
+	}, 
+	{ versionKey: false }
+);
+
+var Category = mongoose.model("Category", categorySchema);
+var Item = mongoose.model("Item", itemSchema);
+ 
+/* DB CONNECTING */
+var options = {};
+options.server = { socketOptions: { keepAlive: 120 }};
+options.replset = { socketOptions: { keepAlive: 120 }};
+options.server = { poolSize: 10 };
+
+mongoose.connect(db.url, options).then(() =>  console.log('DB connection successful...'))
+ 			 			                     .catch((err) => console.error(err));
+
+
+
+
+/* API SECTION */
+router.get('/', function(req, res, next) {
+  res.send('Express REST API is running...');
+});
+
+/* GET ALL ITEM */
+router.get('/item', function(req, res, next) {
+	Item.find({}, function (err, data) {
+    				if (err) return next(err);
+    				res.json(data);
+  	});
+});
+
+/* SAVE ITEM */
+router.post('/item', function(req, res, next) {
+   	console.log(req.body);
+  	Item.create(req.body, function (err, data) {
+    						if (err) return next(err);
+    						res.json({ ok: "Item this name: '" + data.name + "' was successfully saved." });
+  	});
+});
+
+/* UPDATE ITEM */
+router.put('/item', function(req, res, next) {
+   	console.log(req.body);
+  	Item.findByIdAndUpdate(req.body.id, req.body, function (err, data) {
+    						if (err) return next(err);
+    						res.json({ ok: "Item this name: '" + data.name + "' was successfully updated." });
+  	});
+});
+
+/* DELETE ITEM */
+router.delete('/item', function(req, res, next) {
+   	console.log(req.body);
+  	Item.findByIdAndRemove(req.body.id, function (err, data) {
+    						if (err) return next(err);
+    						res.json({ ok: "Item this name: '" + data.name + "' was successfully removed." });
+  	});
+});
+
+/* GET ALL CATEGORY */
+router.get('/category', function(req, res, next) {
+  Category.find({}, function (err, data) {
+            if (err) return next(err);
+            res.json(data);
+    });
+});
+
+/* SAVE CATEGORY */
+router.post('/category', function(req, res, next) {
+    console.log(req.body);
+    Category.create(req.body, function (err, data) {
+                if (err) return next(err);
+                res.json({ ok: "Category this name: '" + data.name + "' was successfully saved." });
+    });
+});
+
+/* DELETE CATEGORY*/
+router.delete('/category', function(req, res, next) {
+    console.log(req.body);
+    Category.findByIdAndRemove(req.body.id, function (err, data) {
+                if (err) return next(err);
+                res.json({ ok: "Category this name: '" + data.name + "' was successfully removed." });
+    });
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,26 +3,28 @@ var path            = require('path');
 var logger 			= require('morgan');
 var bodyParser 		= require('body-parser');
 var app 			= express();
+var api 			= require('./api/api');
 
 app.use(logger('dev')); // выводит все запросы со статусами в консоль
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, "dist")));
+app.use('/api', api);
 
 app.listen(2525, function(){
     console.log('Express server: listening on port 2525...');
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // render the error page
-  res.status(500);
-  res.render('error');
-});
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // render the error page
+//   res.status(500);
+//   res.render('error');
+// });
