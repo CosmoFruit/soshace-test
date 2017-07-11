f1d81bcb3fcf039ab2b8891bd352b10efbff31e3
CosmoFruit <nutchfel@ya.ru>
2017-07-11 13:30:20 +0300

    Создан веб-сервер

---
+++ server.js
@@ -0,0 +1,28 @@
+var express         = require('express');
+var path            = require('path'); 
+var logger 			= require('morgan');
+var bodyParser 		= require('body-parser');
+var app 			= express();
+
+app.use(logger('dev')); // выводит все запросы со статусами в консоль
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.listen(2525, function(){
+    console.log('Express server: listening on port 2525...');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // render the error page
+  res.status(500);
+  res.render('error');
+});
