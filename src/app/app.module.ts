22460f1b6d7d49e1a1921c7066b4b1dc2e86a357
CosmoFruit <nutchfel@ya.ru>
2017-07-12 12:41:06 +0800

    Создана структура клиента

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { CategoryListComponent } from './category-list/category-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemListComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule
